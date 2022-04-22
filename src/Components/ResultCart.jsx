19ee4e4d23604452a243500bac7ed24e3d3025a2
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-22 14:53:58 +0300

    add component

--- src/Components/ResultCart.jsx
+++ src/Components/ResultCart.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const ResultCart = () => {
+const ResultCart = ({ movie }) => {
+  console.log(movie);
   return (
-    <div>ResultCart</div>
-  )
-}
+    <div className="result-card">
+    <div className="poster-wrapper">
+      {movie.poster_path ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt={`${movie.poster_path}`}
+        />
+      ) : (
+        <div className="filler-poster"></div>
+      )}
+    </div>
+    </div>
+  );
+};
 
-export default ResultCart
+export default ResultCart;
