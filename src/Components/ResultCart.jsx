4701272632b3f140e835d06b23c6b897b77b6a05
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-22 15:37:36 +0300

    add context

--- src/Components/ResultCart.jsx
+++ src/Components/ResultCart.jsx
@@ -4,16 +4,32 @@ const ResultCart = ({ movie }) => {
   console.log(movie);
   return (
     <div className="result-card">
-    <div className="poster-wrapper">
-      {movie.poster_path ? (
-        <img
-          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-          alt={`${movie.poster_path}`}
-        />
-      ) : (
-        <div className="filler-poster"></div>
-      )}
-    </div>
+      <div className="poster-wrapper">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+            alt={`${movie.poster_path}`}
+          />
+        ) : (
+          <div className="filler-poster"></div>
+        )}
+      </div>
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.title}</h3>
+          <h4 className="release-date">
+            {movie.release_date
+              ? movie.release_date.substring(0, 4)
+              : "Bilgisi Yok"}
+          </h4>
+          <h4 className="release-date">
+            IMDB: <b>{movie.vote_average ? movie.vote_average : "-"}</b>
+          </h4>
+        </div>
+        <div className="controls">
+          <button className="btn">Add to Watchlist</button>
+        </div>
+      </div>
     </div>
   );
 };
