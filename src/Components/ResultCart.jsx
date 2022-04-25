92fa2fcd2ef5a70301836bd289523533cb17ba94
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-25 11:36:17 +0300

    add changes

--- src/Components/ResultCart.jsx
+++ src/Components/ResultCart.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const ResultCart = ({ movie }) => {
-  console.log(movie);
+  const { watchlist, watched, addMovieToWatchlist, addMovieToWatched } =
+    useContext(GlobalContext);
+
+  const storedMovieWatched = watched.find((o) => o.id === movie.id);
+  const storedMovie = watchlist.find((o) => o.id === movie.id)
+    ? true
+    : !!storedMovieWatched;
   return (
     <div className="result-card">
       <div className="poster-wrapper">
@@ -14,20 +21,33 @@ const ResultCart = ({ movie }) => {
           <div className="filler-poster"></div>
         )}
       </div>
+
       <div className="info">
         <div className="header">
           <h3 className="title">{movie.title}</h3>
           <h4 className="release-date">
-            {movie.release_date
-              ? movie.release_date.substring(0, 4)
-              : "Bilgisi Yok"}
+            {movie.release_date ? movie.release_date.substring(0, 4) : "-"}
           </h4>
           <h4 className="release-date">
             IMDB: <b>{movie.vote_average ? movie.vote_average : "-"}</b>
           </h4>
         </div>
+
         <div className="controls">
-          <button className="btn">Add to Watchlist</button>
+          <button
+            className="btn"
+            disabled={storedMovie}
+            onClick={() => addMovieToWatchlist(movie)}
+          >
+            Add to Watchlist
+          </button>
+          <button
+            className="btn"
+            disabled={storedMovieWatched}
+            onClick={() => addMovieToWatched(movie)}
+          >
+            Add to Watched
+          </button>
         </div>
       </div>
     </div>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AppReducer from "./AppReducer";
 
 //! context olusturma
@@ -12,15 +12,45 @@ const initialState = {
 //! provider components
 export const GlobalProvider = (props) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
-
-    console.log(state)
-    return(
-        // bunu ben kapsadigim zaman neyle kapsarsam altındaki tüm componentlerde burda verirsem onu alır.
-        // <GlobalContext.Provider> bu parent bir kapsayıcı olur. Provider'ı sarmaladık.
-        <GlobalContext.Provider value={{
-            Gokalp: "developer",
-        }}> 
-            {props.children} 
-        </GlobalContext.Provider>
-    )
-}
+  
+    useEffect(() => {
+      localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+      localStorage.setItem("watched", JSON.stringify(state.watched));
+    }, [state]);
+  
+    const addMovieToWatchlist = (movie) => {
+      dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
+    };
+  
+    const removeMovieFromWatchlist = (id) => {
+      dispatch({ type: "REMOVE_MOVIE_FROM_WATCHLIST", payload: id });
+    };
+  
+    const addMovieToWatched = (movie) => {
+      dispatch({ type: "ADD_MOVIE_TO_WATCHED", payload: movie });
+    };
+  
+    const moveToWatchlist = (movie) => {
+      dispatch({ type: "MOVE_TO_WATCHED", payload: movie });
+    };
+  
+    const removeMovieFromWatched = (id) => {
+      dispatch({ type: "REMOVE_MOVIE_FROM_WATCHED", payload: id });
+    };
+  
+    return (
+      <GlobalContext.Provider
+        value={{
+          watchlist: state.watchlist,
+          watched: state.watched,
+          addMovieToWatchlist,
+          removeMovieFromWatchlist,
+          addMovieToWatched,
+          moveToWatchlist,
+          removeMovieFromWatched,
+        }}
+      >
+        {props.children}
+      </GlobalContext.Provider>
+    );
+  };
